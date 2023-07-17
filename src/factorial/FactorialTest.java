a5bc1014a3e03022194345dead8698c57cffe8b6
Anupama <anupama.lakshmi@Improve10x.com>
2023-07-17 15:59:39 +0530

    Added FizzBuzz

--- src/factorial/FactorialTest.java
+++ src/factorial/FactorialTest.java
@@ -1,10 +1,19 @@
 package factorial;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class FactorialTest {
+
+    private Factorial factorial;
+
+    @BeforeEach
+
+    public void setup() {
+        factorial = new Factorial();
+    }
 
     @Test
     public void nothing() {
@@ -13,49 +22,42 @@ public class FactorialTest {
 
     @Test
     public void givenNegativeFactorial_returnNegativeNumber() {
-        Factorial factorial = new Factorial();
         int negativeNumber = factorial.findFactorial(-10);
         assertEquals(-1, negativeNumber);
     }
 
     @Test
     public void givenFactorialNegativeOne_returnNegativeNumber() {
-        Factorial factorial = new Factorial();
         int negativeNumber = factorial.findFactorial(-1);
         assertEquals(-1, negativeNumber);
     }
 
     @Test
     public void givenZeroFactorial_returnZero() {
-        Factorial factorial = new Factorial();
         int ZeroNumber = factorial.findFactorial(0);
         assertEquals(1, ZeroNumber);
     }
 
     @Test
     public void givenOneFactorial_returnOne() {
-        Factorial factorial = new Factorial();
         int number = factorial.findFactorial(1);
         assertEquals(1,number);
     }
 
     @Test
     public void givenTwoFactorial_returnTwo() {
-        Factorial factorial = new Factorial();
         int result = factorial.findFactorial(2);
         assertEquals(2,result);
     }
 
     @Test
     public void givenFourFactorial_returnTwentyFour() {
-        Factorial factorial = new Factorial();
         int result = factorial.findFactorial(4);
         assertEquals(24, result);
     }
 
     @Test
     public void givenFiveFactorial_returnOneHundredTwenty() {
-        Factorial factorial = new Factorial();
         int result = factorial.findFactorial(5);
         assertEquals(120, result);
     }
--- src/fizzbuzz/FizzBuzzTest.java
+++ src/fizzbuzz/FizzBuzzTest.java
@@ -1,10 +1,18 @@
 package fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
 
     @Test
     public void nothing(){
@@ -12,38 +20,52 @@ public class FizzBuzzTest {
     }
 
     @Test
+    public void givenNegative_returnNegativeNumber() {
+        String result = fizzBuzz.findFizzBuzz(-5);
+        assertEquals("-1", result);
+
+    }
+
+    @Test
     public void givenZero_returnEmpty() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.findFizzBuzz(0);
         assertEquals("-1", result);
     }
 
     @Test
     public void givenThree_returnFizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.findFizzBuzz(3);
         assertEquals("Fizz", result);
     }
 
     @Test
     public void givenFive_returnBuzz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.findFizzBuzz(5);
         assertEquals("Buzz", result);
     }
 
     @Test
     public void givenThreeFive_returnFizzBuzz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.findFizzBuzz(15);
         assertEquals("FizzBuzz", result);
     }
 
     @Test
     public void givenNumberEitherMultipleOfThreeOrFive_returnNumber() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.findFizzBuzz(4);
         assertEquals("4", result);
 
+    }
+
+    @Test
+    public void givenTwoDigitNumber_returnFizz() {
+        String result = fizzBuzz.findFizzBuzz(18);
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void givenTwoDigitNumber_returnNumber() {
+        String result = fizzBuzz.findFizzBuzz(22);
+        assertEquals("22", result);
     }
 }
