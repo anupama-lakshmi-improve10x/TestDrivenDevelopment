e38a2fa86ea642f106c62a9acb7b8827ec6fe446
Anupama <anupama.lakshmi@Improve10x.com>
2023-07-18 18:12:22 +0530

    Modified testcases TwoMakesTen

--- src/twomakesten/TwoMakesTenTest.java
+++ src/twomakesten/TwoMakesTenTest.java
@@ -1,10 +1,19 @@
 package twomakesten;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class TwoMakesTenTest {
+
+    private  TwoMakesTen twoMakesTen;
+
+    @BeforeEach
+
+    public void setup() {
+        twoMakesTen = new TwoMakesTen();
+    }
 
     @Test
     public void nothing() {
@@ -21,30 +30,37 @@ public class TwoMakesTenTest {
      */
 
     @Test
-    public void givenNumbersZero_returnFalse() {
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
-        boolean result = twoMakesTen.findTwoMakesTen(0, 0);
-        assertEquals(false, result);
-    }
-
-    @Test
     public void givenNumberNegativeNumberAndOne_returnFalse() {
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean result = twoMakesTen.findTwoMakesTen(-1, 0);
         assertEquals(false, result);
     }
 
     @Test
     public void givenTwoNumbersNegative_returnFalse() {
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean result = twoMakesTen.findTwoMakesTen(-1, -1);
         assertEquals(false, result);
+    }
 
+    @Test
+    public void givenOneNegativeAndOtherNumbersPositive_returnFalse() {
+        boolean result = twoMakesTen.findTwoMakesTen(-10, 5);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void givenOneNegativeAndOtherNumbersNegative_returnFalse() {
+        boolean result = twoMakesTen.findTwoMakesTen(-5, -5);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void givenNumbersZero_returnFalse() {
+        boolean result = twoMakesTen.findTwoMakesTen(0, 0);
+        assertEquals(false, result);
     }
 
     @Test
     public void givenNumberTenAndZero_returnTrue() {
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean result = twoMakesTen.findTwoMakesTen(10, 0);
         assertEquals(true, result);
     }
@@ -61,6 +77,5 @@ public class TwoMakesTenTest {
         TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean result = twoMakesTen.findTwoMakesTen(9, 1);
         assertEquals(true, result);
-
     }
 }
