ec6048731b91c84311278b22d401ab037217f554
Anupama <anupama.lakshmi@Improve10x.com>
2023-08-05 22:00:57 +0530

    Completed SortArrayNumber in array using TDD

--- src/sortedarray/SortArray.java
+++ src/sortedarray/SortArray.java
@@ -11,7 +11,7 @@ public class SortArray {
             result = true;
         } else if(arr.length > 1) {
             for(int i = arr.length -1; i >= 0; i--) { // 4, 4>=0, 3
-                for(int j = 0; j < i; j++) {
+                for(int j = 0; j < i; j++) { // 0, 0 <3,
                     if(arr[i] < arr[j]) {
                         return false;
                     }
