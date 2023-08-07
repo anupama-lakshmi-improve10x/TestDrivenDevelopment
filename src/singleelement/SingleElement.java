ee87bf10538b7c50d6d7d99b35fe1c183527a0a9
Anupama <anupama.lakshmi@Improve10x.com>
2023-08-07 12:00:55 +0530

    Completed SingleElement problem using TDD

--- src/singleelement/SingleElement.java
+++ src/singleelement/SingleElement.java
@@ -2,7 +2,24 @@ package singleelement;
 
 public class SingleElement {
     public int getSingleElement(int[] arr) {
-
+        if(arr == null  || arr.length == 0) {
+            return -1;
+        } else if(arr.length == 1) {
+            return 1;
+        }
+        int n = arr.length;
+        for(int i = 0; i < n; i++) {
+            int num = arr[i];
+            int count = 0;
+            for(int j= 0; j < n; j++) {
+                if(arr[j] == num) {
+                    count++;
+                }
+            }
+            if(count == 1) {
+                return num;
+            }
+        }
         return -1;
     }
 }
--- src/singleelement/SingleElementTest.java
+++ src/singleelement/SingleElementTest.java
@@ -16,4 +16,46 @@ public class SingleElementTest {
         int nullValue = singleElement.getSingleElement(null);
         Assertions.assertEquals(-1, nullValue);
     }
+
+    @Test
+    public void givenEmpty_returnNegativeOne() {
+        SingleElement singleElement = new SingleElement();
+        int emptyValue = singleElement.getSingleElement(new int[] {});
+        Assertions.assertEquals(-1, emptyValue);
+    }
+
+    @Test
+    public void givenZero_returnOne() {
+        SingleElement singleElement = new SingleElement();
+        int zeroValue = singleElement.getSingleElement(new int[] {0});
+        Assertions.assertEquals(1, zeroValue);
+    }
+
+    @Test
+    public void givenOne_returnOne() {
+        SingleElement singleElement = new SingleElement();
+        int oneValue = singleElement.getSingleElement(new int[] {1});
+        Assertions.assertEquals(1, oneValue);
+    }
+
+    @Test
+    public void givenTwoOnes_returnOne() {
+        SingleElement singleElement = new SingleElement();
+        int twoOnesValue = singleElement.getSingleElement(new int[] {1, 1});
+        Assertions.assertEquals(-1, twoOnesValue);
+    }
+
+    @Test
+    public void givenTwoOnesAndThree_returnThree() {
+        SingleElement singleElement = new SingleElement();
+        int twoOnesAndThreeValue = singleElement.getSingleElement(new int[] {1, 1, 3});
+        Assertions.assertEquals(3, twoOnesAndThreeValue);
+    }
+
+    @Test
+    public void givenArray_returnSeven() {
+        SingleElement singleElement = new SingleElement();
+        int twoOnesAndThreeValue = singleElement.getSingleElement(new int[] {1, 1, 3, 3, 4, 4, 4, 7});
+        Assertions.assertEquals(7, twoOnesAndThreeValue);
+    }
 }
