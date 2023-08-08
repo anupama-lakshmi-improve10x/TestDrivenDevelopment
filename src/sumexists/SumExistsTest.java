caa6758964079d517441d830be6c5c6087276f59
Anupama <anupama.lakshmi@Improve10x.com>
2023-08-08 15:52:54 +0530

    Started SumExists using TDD

--- src/sumexists/SumExistsTest.java
+++ src/sumexists/SumExistsTest.java
@@ -1,11 +1,19 @@
 package sumexists;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SumExistsTest {
+
+    private SumExists sumExists;
+
+    @BeforeEach
+    public void setup() {
+        sumExists = new SumExists();
+    }
 
     @Test
     public void nothing() {
@@ -13,91 +21,78 @@ public class SumExistsTest {
 
     @Test
     public void givenNull_returnNo() {
-        SumExists sumExists = new SumExists();
         String nullValue = sumExists.sumOfTwoArrays(null, 0);
         assertEquals("No", nullValue);
     }
 
     @Test
     public void givenEmpty_returnNo() {
-        SumExists sumExists = new SumExists();
         String emptyValue = sumExists.sumOfTwoArrays(new int[] {}, 0);
         assertEquals("No", emptyValue);
     }
 
     @Test
     public void givenOneValue_returnNo() {
-        SumExists sumExists = new SumExists();
         String oneValue = sumExists.sumOfTwoArrays(new int[] {1}, 0);
         assertEquals("No", oneValue);
     }
 
     @Test
     public void givenZeroOneValue_returnNo() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {0}, 0);
         assertEquals("No", array);
     }
 
     @Test
     public void givenZeroOneValue_returnYes() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {0,1}, 1);
         assertEquals("Yes", array);
     }
 
     @Test
     public void given012_returnNo() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {0, 1, 2}, 4);
         assertEquals("No", array);
     }
 
     @Test
     public void given265811_returnYes() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {2,6,5,8,11}, 14);
         assertEquals("Yes", array);
     }
 
     @Test
     public void given265811_returnNo() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {2,6,5,8,11}, 9);
         assertEquals("No", array);
     }
 
     @Test
     public void given0234_returnYes() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {0,2,3,4}, 4);
         assertEquals("Yes", array);
     }
 
     @Test
     public void given1234_returnYes() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {1,2,3,4}, 4);
         assertEquals("Yes", array);
     }
 
     @Test
     public void given234_returnNo() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {2,3,4}, 4);
         assertEquals("No", array);
     }
 
     @Test
     public void givenNegativeValue_returnYes() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {-2,-2,0}, -4);
         assertEquals("Yes", array);
     }
 
     @Test
     public void givenNegativeValue_returnNo() {
-        SumExists sumExists = new SumExists();
         String array = sumExists.sumOfTwoArrays(new int[] {-2,-1,0}, -8);
         assertEquals("No", array);
     }
