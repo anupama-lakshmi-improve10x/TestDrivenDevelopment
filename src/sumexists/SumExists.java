5deaff70dee29193f7195c2b4be2c400be14cfe5
Anupama <anupama.lakshmi@Improve10x.com>
2023-08-08 15:00:55 +0530

    Started SumExists using TDD

---
+++ src/sumexists/SumExists.java
@@ -0,0 +1,7 @@
+package sumexists;
+
+public class SumExists {
+    public String sumOfTwoArrays(int[] arr, int target) {
+        return "No";
+    }
+}
--- src/sumexists/SumExistsTest.java
+++ src/sumexists/SumExistsTest.java
@@ -1,10 +1,20 @@
 package sumexists;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SumExistsTest {
 
     @Test
     public void nothing() {
+    }
+
+    @Test
+    public void givenNull_returnNo() {
+        SumExists sumExists = new SumExists();
+        String nullValue = sumExists.sumOfTwoArrays(null, 0);
+        assertEquals("No", nullValue);
     }
 }
