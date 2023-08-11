4edcce26b0ff47c863b40dcdca07aab8e6f4fe1f
Anupama <anupama.lakshmi@Improve10x.com>
2023-08-11 12:14:22 +0530

    Started Leaders Using TDD

--- src/leaders/Leaders.java
+++ src/leaders/Leaders.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Leaders {
     public ArrayList<Integer> findLeaders(int[] num, int n) {
-        if(num == null) {
+        if(num == null || num.length == 0) {
             return new ArrayList<>();
         }
         return null;
